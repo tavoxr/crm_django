4df1ce53b4987bf35d9bea7872a361b1d1a29d0d
tavoxr <tavoxr23@gmail.com>
2021-02-20 13:18:05 -0600

    reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns=[
@@ -14,5 +14,14 @@ urlpatterns=[
     path('ordersCreate/<int:pk>/', views.createOrder, name="orders"),
     path('orders/<int:pk>/', views.updateOrder, name='updateOrder'),
     path('ordersDelete/<int:pk>', views.deleteOrder, name="deleteOrder"),
-    path('account_settings/', views.accountSettings, name="account_settings")
+    path('account_settings/', views.accountSettings, name="account_settings"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), name='password_reset'),  # utilizando las clases de django para resetear pass por correo se pone as_view() porque es una base class view
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name= "accounts/password_reset_sent.html"), name='password_reset_done'), # render succes message when password was set
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name= "accounts/password_reset_form.html"), name='password_reset_confirm'), 
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), name='password_reset_complete'), 
 ]
+
+#submit email form                          //PasswordResetView
+#Email sent success message                 //PasswordResetDoneView
+#Link to password Reset form in email       //PasswordResetConfirmView
+#Password successfully changed message      //
--- app/settings.py
+++ app/settings.py
@@ -137,3 +137,12 @@ STATICFILES_DIRS=[
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+#SMTP  Configuration
+#configrando email para permitir el cambio de passwords
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_PORT="587"
+EMAIL_USE_TLS= True
+EMAIL_HOST_USER="tavoxdev@gmail.com"
+EMAIL_HOST_PASSWORD="Evangelion01"
